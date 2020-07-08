4afb3517bf81101e124535f56bd24269e467df7f
Alexander Chaschin <ac@happynumbers.com>
2020-07-09 00:45:32 +0300

    fix migrations

--- db/migrate/20200708162430_create_tests.rb
+++ db/migrate/20200708162430_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.text :title
-      t.integer :category
+      t.integer :level
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20200708213028_add_null_constraints.rb
+++ db/migrate/20200708213028_add_null_constraints.rb
@@ -3,7 +3,7 @@ class AddNullConstraints < ActiveRecord::Migration[6.0]
     change_column_null(:tests, :title, false)
     change_column_null(:questions, :body, false)
     change_column_null(:answers, :body, false)
-    change_column_null(:categories, :body, false)
+    change_column_null(:categories, :name, false)
 
     change_column_null(:users, :name, false)
     change_column_null(:users, :type, false)
