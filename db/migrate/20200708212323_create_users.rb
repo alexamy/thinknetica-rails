f0dd495cad41d32d1baa98527fae603628069750
Alexander Chaschin <ac@happynumbers.com>
2020-07-09 00:23:59 +0300

    add user model

---
+++ db/migrate/20200708212323_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.text :name
+      t.text :type
+      t.text :email
+      t.text :password_digest
+
+      t.timestamps
+    end
+  end
+end
