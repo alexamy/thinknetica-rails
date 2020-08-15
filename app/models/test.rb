3084b89db8b516e7ad97d33613247e9dd3eea3a1
Alexander Chaschin <achasch@gmail.com>
2020-08-15 20:27:54 +0300

    03. Интерфейс запросов ActiveRecord (#8)
    feat: add User and Test helper methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :user_tests
+  has_many :users, :through => :user_tests
+
+  def self.by_category(category_name)
+    Test
+      .joins(:category)
+      .where(categories: { name: category_name })
+      .order("title DESC")
+      .pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Категория Культура
+category = Category.create(name: "Культура")
+
+## Известные личности
+test = Test.create(title: "Известные личности", level: 2, category: category)
+
+question = Question.create(body: "Год рождения А. В. Суворова?", test: test)
+Answer.create(body: "1730", correct: true, question: question)
+
+question = Question.create(body: "Первый роман Ф. М. Достоевского?", test: test)
+Answer.create(body: "Бедные люди", correct: true, question: question)
+
+
+
+# Категория Программирование
+category = Category.create(name: "Программирование")
+
+## Javascript
+test = Test.create(title: "Javascript", level: 1, category: category)
+
+question = Question.create(body: "Является ли Array типом данных?", test: test)
+Answer.create(body: "Нет", correct: true, question: question)
+
+question = Question.create(body: "Позволяет ли объявление переменной словом const её изменение?", test: test)
+Answer.create(body: "Нет", correct: true, question: question)
+
+
+## Haskell
+test = Test.create(title: "Haskell", level: 2, category: category)
+
+question = Question.create(body: "Какой тип свёртки позволяет обрабатывать бесконечные списки?", test: test)
+Answer.create(body: "foldr", correct: true, question: question)
+
+question = Question.create(body: "Какова максимальная арность функций?", test: test)
+Answer.create(body: "1", correct: true, question: question)
+
+
+
+# Пользователи
+User.create(name: "Jonh Doe", email: "johndoe@example.com", user_type: "student", password_digest: "md5test1")
+User.create(name: "Mike Doe", email: "mikedoe@example.com", user_type: "student", password_digest: "md5test2")
+
+user = User.create(name: "Teresa Doe", email: "teresadoe@example.com", user_type: "teacher", password_digest: "md5test3")
+user.tests << Test.all
+user.save
