482b6378c29b895c4f645dde2a6f92c7049e216f
Alexander Chaschin <chaschin@uchi.ru>
2020-08-15 21:32:48 +0300

    fix hash style in test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :user_tests
-  has_many :users, :through => :user_tests
+  has_many :users, through: :user_tests
 
   def self.by_category(category_name)
     Test
