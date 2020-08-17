e6a7b6f4cd56b451a9014957de95580d72c642db
Alexander Chaschin <chaschin@uchi.ru>
2020-08-18 00:05:04 +0300

    add uniqueness validation to test by title and level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,7 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :easy,   -> { where(level: 0..1) }
