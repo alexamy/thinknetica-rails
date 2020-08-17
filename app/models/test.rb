3cf9b74194662e114b8859793b29d187384c4587
Alexander Chaschin <chaschin@uchi.ru>
2020-08-17 23:50:45 +0300

    add presence validation

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,8 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :title, presence: true
+
   scope :easy,   -> { where(level: 0..1) }
   scope :normal, -> { where(level: 2..4) }
   scope :hard,   -> { where(level: 5..)  }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :user_tests
   has_many :own_tests, class_name: 'Test', foreign_key: 'author_id'
 
+  validates :name, :email, presence: true
+
   def tests_by_level(level)
     tests.where(level: level)
   end
