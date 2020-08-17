65c8e52b564b95b3a4f3612d3446c53b1daf5060
Alexander Chaschin <chaschin@uchi.ru>
2020-08-18 00:18:26 +0300

    add question answer count validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
+  has_many :answers
 
   validates :body, presence: true
+  validate :validate_answer_count
+
+  private
+
+  def validate_answer_count
+    errors.add(:answers, "должны быть между 1 и 4") unless answers.length.between?(1, 4)
+  end
 end
