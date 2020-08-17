74df8e9af877a1a64c087496f33888ee0f83ccdd
Alexander Chaschin <chaschin@uchi.ru>
2020-08-17 23:25:46 +0300

    add test difficulty scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  scope :easy,   -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard,   -> { where(level: 5..)  }
+
   def self.by_category(category_name)
     Test
       .joins(:category)
