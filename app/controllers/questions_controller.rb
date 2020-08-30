943da1510a1a0ade6c16854ce963b32ee125dfc3
Alexander Chaschin <chaschin@uchi.ru>
2020-08-30 17:50:41 +0300

    add actions in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,22 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
+  before_action :scope_questions
+
   def index
-    render plain: Test.find(params[:test_id]).questions.to_json
+    render plain: @questions.to_json
   end
 
   def show
     render plain: Question.find(params[:id]).to_json
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id]) if params[:test_id]
+  end
+
+  def scope_questions
+    @questions = @test ? @test.questions : Question.all
   end
 end
