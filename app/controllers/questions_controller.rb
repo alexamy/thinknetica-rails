87c7165e9af68f144ea42b8c7d9ac484458126c2
Alexander Chaschin <chaschin@uchi.ru>
2020-08-30 17:34:20 +0300

    add index and show boilerplates

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,9 @@
 class QuestionsController < ApplicationController
+  def index
+    render plain: Test.find(params[:test_id]).questions.to_json
+  end
+
+  def show
+    render plain: Question.find(params[:id]).to_json
+  end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,4 +2,8 @@ class TestsController < ApplicationController
   def index
     render plain: Test.all.to_json
   end
+
+  def show
+    render plain: Test.find(params[:id]).to_json
+  end
 end
