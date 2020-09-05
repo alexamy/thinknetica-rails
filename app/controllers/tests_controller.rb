7157cbbee013e2be6d7d84406465937ca822b496
Alexander Chaschin <chaschin@uchi.ru>
2020-09-05 13:04:45 +0300

    add test show view

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,16 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show]
+
   def index
     render plain: Test.all.to_json
   end
 
   def show
-    render plain: Test.find(params[:id]).to_json
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
