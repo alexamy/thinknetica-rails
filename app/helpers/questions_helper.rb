1bae0a31a0bf70e51251406aa9d95797627f5f55
Alexander Chaschin <chaschin@uchi.ru>
2020-09-06 00:23:01 +0300

    add form header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
 module QuestionsHelper
+  def form_header
+    case action_name
+    when 'new'
+      'Создание нового вопроса'
+    when 'edit'
+      'Редактирование вопроса'
+    else
+      raise 'Unknown action'
+    end
+  end
 end
