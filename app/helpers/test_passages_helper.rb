29019d2d41034282e9191e75b9fad3f3db316ddf
Alexander Chaschin <chaschin@uchi.ru>
2020-09-09 11:10:29 +0300

    add question position

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,9 @@
 module TestPassagesHelper
+  def current_question_index(test_passage)
+    test_passage.test.questions.ids.index(test_passage.current_question.id) + 1
+  end
+
+  def questions_size(test_passage)
+    test_passage.test.questions.size
+  end
 end
