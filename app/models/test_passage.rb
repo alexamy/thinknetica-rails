4daf9995e6c416a60c819e986ee3a0b82e4d6232
Alexander Chaschin <chaschin@uchi.ru>
2020-09-10 01:26:53 +0300

    fiix test passage current question validation

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_set_next_question
+  before_validation :before_validation_set_current_question
 
   SUCCESSFUL_PERCENT_MARGIN = 85
 
@@ -35,12 +34,12 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
-  def before_update_set_next_question
-    self.current_question = next_question
+  def before_validation_set_current_question
+    if self.current_question
+      self.current_question = next_question
+    else
+      self.current_question = test.questions.first if test.present?
+    end
   end
 
   def correct_answer?(answer_ids)
