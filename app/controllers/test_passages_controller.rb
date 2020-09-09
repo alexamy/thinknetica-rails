00f6e7c6e6462111980bf38bde331ef41d0edac2
Alexander Chaschin <chaschin@uchi.ru>
2020-09-09 10:16:44 +0300

    add test_passage show

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,5 +1,7 @@
 class TestPassagesController < ApplicationController
 
+  before_action :set_test_passage, only: %i[show result update]
+
   def show
   end
 
@@ -7,6 +9,12 @@ class TestPassagesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def set_test_passage
+    @test_passage = TestPassage.find(params[:id])
   end
 
 end
