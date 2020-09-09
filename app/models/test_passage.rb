5292dbaa5db10b47ee5a8a56d280f5ae5ecfee82
Alexander Chaschin <chaschin@uchi.ru>
2020-09-09 15:34:10 +0300

    more concise correct answer check

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -38,7 +38,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    return false unless answer_ids
+    return false if answer_ids.blank?
 
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
