64febffadef93a49e76b1d5002e415714ea80219
Alexander Chaschin <chaschin@uchi.ru>
2020-09-15 15:32:22 +0300

    add navbar welcome message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
     unless current_user
       redirect_to login_path, alert: 'Доступ запрещен. Введите email и пароль.'
     end
+
+    cookies[:email] = current_user&.email
   end
 
   def current_user
