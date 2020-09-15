19da7432bcafd0b68fe1324f81b4f1b3174c84d4
Alexander Chaschin <chaschin@uchi.ru>
2020-09-15 15:14:05 +0300

    add flash alert for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Ошибка авторизации. Проверьте пароль и/или email.'
       render :new
     end
   end
