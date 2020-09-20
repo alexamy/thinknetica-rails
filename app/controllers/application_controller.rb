d8ad02fc4ee5ac48349b9b6de466c844acd26bad
Alexander Chaschin <chaschin@uchi.ru>
2020-09-21 01:28:36 +0300

    add custom fields to devise user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :user_type, :email, :password)
+      end
+
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :user_type, :email, :password)
+      end
+  end
 end
