167e481ad8b9297d88cfa6a3ab8de62c9b346b2c
Alexander Chaschin <chaschin@uchi.ru>
2020-09-22 01:58:28 +0300

    add devise redirection based on user type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_tests_path
+    else
+      root_path
+    end
+  end
+
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:first_name, :last_name, :type, :email, :password)
