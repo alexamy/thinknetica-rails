d01147d8ab7582a5de165fbd0015fa95929de82e
Alexander Chaschin <chaschin@uchi.ru>
2020-09-22 03:18:15 +0300

    remove show actions on questions and answers for user

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests, only: :index do
-    resources :questions, only: :show, shallow: true do
-      resources :answers, only: :show, shallow: true
-    end
-
     post :start, on: :member
   end
 
