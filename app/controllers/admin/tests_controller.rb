6c6a455f63f1351d58b134eb0d84b4410f59d3c2
Alexander Chaschin <chaschin@uchi.ru>
2020-09-29 23:18:53 +0300

    fix forms

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -3,7 +3,6 @@ class Admin::TestsController < Admin::BaseController
   before_action :set_test, only: %i[show edit update destroy start update_inline]
 
   def index
-    @tests = Test.all
   end
 
   def show
