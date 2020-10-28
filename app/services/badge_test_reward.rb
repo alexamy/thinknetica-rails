c23cd6aebc96d179a646d97d068bfc01566fdec1
Alexander Chaschin <chaschin@uchi.ru>
2020-10-28 10:39:04 +0300

    fix badge test reward

--- app/services/badge_test_reward.rb
+++ app/services/badge_test_reward.rb
@@ -10,35 +10,32 @@ class BadgeTestReward < ApplicationService
     return unless @test_passage.completed?
 
     @badges.each do |badge|
-      can_reward = find_test_first_try(badge) && find_category(badge) && find_level(badge)
-      user.badges << badge if can_reward
+      can_reward = find_test_first_try(badge) || find_category(badge) || find_level(badge)
+      @user.badges << badge if can_reward
     end
   end
 
   private
 
   def find_test_first_try(badge)
-    return true unless badge.test_first_try
     return unless badge.test_first_try == @test
 
     @user.test_passages.where(test: badge.test_first_try).count == 1
   end
 
   def find_category(badge)
-    return true unless badge.category
     return unless badge.category == @test.category
 
-    tests_user = user.test_passages.map { |passage| passage.test.id }.uniq.sort
+    tests_user = @user.test_passages.map { |passage| passage.test.id }.uniq.sort
     tests_all = Test.where(category: badge.category).map(&:id).sort
 
     tests_user == tests_all
   end
 
   def find_level(badge)
-    return true unless badge.level.positive?
     return unless badge.level == @test.level
 
-    tests_user = user.test_passages.map { |passage| passage.test.id }.uniq.sort
+    tests_user = @user.test_passages.map { |passage| passage.test.id }.uniq.sort
     tests_all = Test.where(level: badge.level).map(&:id).sort
 
     tests_user == tests_all
